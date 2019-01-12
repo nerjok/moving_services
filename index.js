dd8d02d0c85a5bba2919c85816c0f58bfb169d4b
Dell <neriejus@gmail.com>
2019-01-12 21:57:49 +0200

    sdfsd

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ require('./routes/billingRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
-    const path = require('path');\
+    const path = require('path');
     app.get('*', (req, res) => {
         res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
